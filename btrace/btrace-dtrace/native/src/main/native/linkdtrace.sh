7e3235ab26905351768cae6cdca042249cb76e42
Jaroslav Bachorik <yardus@netbeans.org>
2011-03-22 14:25:47 +0100

    fixing the part responsible for building native dtrace libraries

--- btrace/btrace-dtrace/native/src/main/native/linkdtrace.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-OUTDIR=$BUILDDIR/objs
-
-/usr/sbin/dtrace -G -o $OUTDIR/btraced.o -s $SRCDIR/btraced.d $OUTDIR/btrace.o
-$CC -G $OUTDIR/btrace.o $OUTDIR/btraced.o -o $BUILDDIR/$LIBNAME-$ARCH.so
