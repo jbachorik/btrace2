28127873a33c53bf9a82fabf8882e2f2d635c0cf
Jaroslav Bachorik <yardus@netbeans.org>
2012-08-16 23:08:02 +0200

    fixing the binary distribution

--- dist/src/bin/btrace.bat
+++ dist/src/bin/btrace.bat
@@ -6,10 +6,18 @@ set DEFAULT_BTRACE_HOME=%~dp0..
 if "%BTRACE_HOME%"=="" set BTRACE_HOME=%DEFAULT_BTRACE_HOME%
 set DEFAULT_BTRACE_HOME=
 
-if not exist "%BTRACE_HOME%\lib\btrace-cli-${project.version}.jar" goto noBTraceHome
+set BTRACE_LIB=%BTRACE_HOME%\lib
+
+if not exist "%BTRACE_LIB%\cli-${project.version}.jar" goto noBTraceHome
 
 if "%JAVA_HOME%" == "" goto noJavaHome
-  "%JAVA_HOME%/bin/java" -Dnet.java.btrace.probeDescPath=. -Dnet.java.btrace.dumpClasses=false -Dnet.java.btrace.debug=false -Dnet.java.btrace.unsafe=false -cp "%BTRACE_HOME%/build/btrace-client.jar;%JAVA_HOME%/lib/tools.jar" net.java.btrace.client.Main %*
+
+set BTRACE_CP=
+for %%a in (%BTRACE_LIB%\*.jar) do (
+    set BTRACE_CP=%BTRACE_CP%;%%a
+)
+
+  "%JAVA_HOME%/bin/java" -Dnet.java.btrace.probeDescPath=. -Dnet.java.btrace.dumpClasses=false -Dnet.java.btrace.debug=false -Dnet.java.btrace.unsafe=false -cp "%BTRACE_CP%;%JAVA_HOME%/lib/tools.jar" net.java.btrace.client.Main %*
   goto end
 :noJavaHome
   echo Please set JAVA_HOME before running this script
--- dist/src/bin/btracec.bat
+++ dist/src/bin/btracec.bat
@@ -6,10 +6,16 @@ set DEFAULT_BTRACE_HOME=%~dp0..
 if "%BTRACE_HOME%"=="" set BTRACE_HOME=%DEFAULT_BTRACE_HOME%
 set DEFAULT_BTRACE_HOME=
 
-if not exist "%BTRACE_HOME%\lib\btrace-core.jar" goto noBTraceHome
+if not exist "%BTRACE_HOME%\lib\core.jar" goto noBTraceHome
 
 if "%JAVA_HOME%" == "" goto noJavaHome
-   "%JAVA_HOME%/bin/java" -cp "%BTRACE_HOME%/build/btrace-cli.jar;%JAVA_HOME%/lib/tools.jar" net.java.btrace.compiler.Compiler %*
+
+set BTRACE_CP=
+for %%a in (%BTRACE_LIB%\*.jar) do (
+    set BTRACE_CP=%BTRACE_CP%;%%a
+)
+
+   "%JAVA_HOME%/bin/java" -cp "%BTRACE_CP%;%JAVA_HOME%/lib/tools.jar" net.java.btrace.compiler.Compiler %*
   goto end
 :noJavaHome
   echo Please set JAVA_HOME before running this script
